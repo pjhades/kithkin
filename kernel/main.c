c1e2a89a3aa24c0955260bbbd5f1320a107c2ad7
pjhades <pj.hades@gmail.com>
2015-06-11 19:08:41 -0300

    Add begin/end address for .text.entry section

--- kernel/main.c
+++ kernel/main.c
@@ -1,26 +1,15 @@
-#include <asm/e820.h>
 #include <kernel/kernel.h>
 #include <kernel/types.h>
 #include <kernel/console.h>
 
-int test;
+extern char _stextentry[], _etextentry[];
 
 void kernel_main(void)
 {
     console_clear_screen();
 
-    cputs("address of test: ");
-    cputhex((uint32_t)&test);
-    cputs("\n");
-    cputs("hello world!\n");
-
-    int a = printk("%d + %d = %d\n", 199, 145, 199 + 145);
-    cputhex(a);
-    printk("\n");
-    unsigned int b = 0x80000000;
-    printk("i: %d\nu: %u\n", b, b);
-    uint64_t c = 0x8000000000000000LL;
-    printk("q: %q\nU: %U\n", c, c);
+    printk("_stextentry=%p\n", _stextentry);
+    printk("_etextentry=%p\n", _etextentry);
 
     while (1);
 }
